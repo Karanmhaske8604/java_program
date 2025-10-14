241dbbf4357bbc21c6140fe49b29170e8df4863c
Karanmhaske8604 <karanmhaske3050@gamil.com>
2025-10-14 15:27:42 +0530

    Constructor

---
+++ methods/Java_method.java
@@ -0,0 +1,30 @@
+package java_home.methods;
+
+public class Java_method {
+
+	public static void main(String[] args) {
+
+		Book book=new Book(10,20);//constructor call.
+		book.display();
+	}
+}
+
+
+class Book
+{
+   public Book() //default constructer.non-parameterized
+   {
+	   System.out.println("good morning");
+   }
+   
+   public Book(int i,int j)//parameter constructer.
+   {
+	   System.out.println(i*j);
+	   System.out.println(i/j);
+   }
+   
+   public void display()
+   {
+	   System.out.println("we will continue...");
+   }
+}
