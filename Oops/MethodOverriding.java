61f96c1ddf8644e16ebf430818c7f67176b98a4a
Karanmhaske8604 <karanmhaske3050@gamil.com>
2025-11-06 15:15:34 +0530

    Overriding

--- Oops/MethodOverriding.java
+++ Oops/MethodOverriding.java
@@ -3,18 +3,45 @@ package java_home.Oops;
 public class MethodOverriding {
 
 	public static void main(String[] args) {
-		
-
+		Earth e=new Earth(8);
+		System.out.println(e.name);
+		e.display();
+		System.out.println(e.sum(25,56));
 	}
 
 }
  
-class Planet
+class Solar
 {
-  public void 
+  public void display()
+  {
+	  
+	 System.out.println("I have the Planet..");
+  }
+  
+  public int sum(int a,int b)
+  {
+	  return a+b;
+  }
+  
 }
 
-class Earth extends Planet
+class Earth extends Solar
 {
+	String name="Solar";
+	public void display()
+	{
+		System.out.println("Im blue");
+	}
+	
+	public int sum(int a,int b)
+	{
+		return a*b;
+	}
+	
+	public Earth(int a)
+	{
+		System.out.println("No. planets in solar "+a);
+	}
 	
 }
