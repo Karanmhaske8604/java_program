3120ddb155b21d045a54ee29c799fefb5d7a9b6a
Karanmhaske8604 <karanmhaske3050@gamil.com>
2025-11-07 15:53:49 +0530

    Polymorphic Object

---
+++ Oops/Polyobj.java
@@ -0,0 +1,38 @@
+package java_home.Oops;
+
+public class Polyobj {
+
+	public static void main(String[] args) {
+		
+		Animals ani=new Dog();//Reference of parent class but object of child class this is polymorphic object.it shows only common methods in between them.  
+		ani.Nature();//compile time shows ref.of parent but gives op of child class at run time
+		//ani.Run();
+		if (ani instanceof Dog)
+		{
+			((Dog) ani).Run();
+		}
+
+	}
+
+}
+
+class Animals
+{
+ public void Nature()
+ {
+	 System.out.println("We have two types");
+ }
+}
+
+class Dog extends Animals
+{
+ public void Nature()
+ {
+	 System.out.println("I am pet Animal");
+ }
+ 
+ public void Run()
+ {
+	 System.out.println("I am also can be wild");
+ }
+}
