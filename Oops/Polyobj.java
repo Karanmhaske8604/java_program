350325b9acb5b4b4f5e0dff483b511cc623a9893
Karanmhaske8604 <karanmhaske3050@gamil.com>
2025-11-28 13:22:00 +0530

    Collection

--- Oops/Polyobj.java
+++ Oops/Polyobj.java
@@ -6,11 +6,11 @@ public class Polyobj {
 		
 		Animals ani=new Dog();//Reference of parent class but object of child class this is polymorphic object.it shows only common methods in between them.  
 		ani.Nature();//compile time shows ref.of parent but gives op of child class at run time
-		//ani.Run();
-		if (ani instanceof Dog)
+		((Dog)ani).Run();//type casting to get extra method of child class
+	/*	if (ani instanceof Dog)
 		{
 			((Dog) ani).Run();
-		}
+		}*/
 
 	}
 
@@ -31,7 +31,7 @@ class Dog extends Animals
 	 System.out.println("I am pet Animal");
  }
  
- public void Run()
+ public void Run()//this is true extensibility in OOP
  {
 	 System.out.println("I am also can be wild");
  }
