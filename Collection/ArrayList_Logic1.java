1c08a582bc556464b36f973a51278a970e69efcc
Karanmhaske8604 <karanmhaske3050@gamil.com>
2025-12-03 16:11:11 +0530

    Arraylist

---
+++ Collection/ArrayList_Logic1.java
@@ -0,0 +1,31 @@
+package java_home.Collection;
+import java.util.*;
+
+public class ArrayList_Logic1 {
+
+	public static void main(String[] args) {
+		
+		List<String>list = new ArrayList<String>();
+		list.add("Rahul");
+		list.add("Abhi");
+		list.add("Keshav");
+		list.add("Karan");
+		
+		System.out.println(list);
+		
+		for(int i=0;i<list.size();i++)
+		{
+			System.out.println(list.get(i));
+			
+		}
+		
+		System.out.println(list.contains("Keshav"));
+		
+		
+		
+		
+		
+
+	}
+
+}
