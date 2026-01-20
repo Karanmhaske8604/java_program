89b0d6392e295678428c3c37fc78f8be166bf4dd
Karanmhaske8604 <karanmhaske3050@gmail.com>
2026-01-21 00:36:52 +0530

    BinaryOperator using

---
+++ StreamOperations/Operation13.java
@@ -0,0 +1,27 @@
+package java_home.StreamOperations;
+
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class Operation13 {
+
+	public static void main(String[] args) {
+		// reduce type 1 : Binary Operator
+		
+		BinaryOperator<Integer> bio=(i,j)->{return i*j;};
+		//0 - Accumulator in Multiplication
+	int mul=List.of(5,15,25,35,45)
+		.stream()
+		.reduce(1,bio);//pass accumulator  
+	System.out.println(mul);	
+	
+	BinaryOperator<String> biOp=(i,j)->{return i+j;};
+	String conct=List.of("10","10","10")
+			.stream()
+			.reduce("",(i,j)->{return i.concat(j)+"####";});
+	System.out.println(conct);
+	
+
+	}
+
+}
