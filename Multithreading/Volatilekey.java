4b9ddd82ca99f7de489f94bc11fd5a3eca0f955f
Karanmhaske8604 <karanmhaske3050@gmail.com>
2026-02-16 16:19:43 +0530

    Volatile

--- Multithreading/Volatilekey.java
+++ Multithreading/Volatilekey.java
@@ -1,12 +1,44 @@
 package java_home.Multithreading;
 
+
 public class Volatilekey {
 
 	public static void main(String[] args) {
+	
+		Thread read=new Thread(()->{
+			System.out.println("Reading flag...");
+			while(flag==true)
+			{
+				
+			}
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Reading Operation ended...");
+				});
 		
+		Thread write=new Thread(()->{
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Setting flag to false...");
+			flag=false;
+			
+		});
+		
+		read.start();
+		write.start();
+		
+		flag=true;
 	}
 	
-	static boolean flag=true;
+	volatile static boolean flag=true;
 	
 
 }
