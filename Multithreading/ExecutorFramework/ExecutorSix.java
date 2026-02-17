472274da956aaf746652657773f6a9f8c97b78e1
Karanmhaske8604 <karanmhaske3050@gmail.com>
2026-02-17 13:17:42 +0530

    Executor Framework

---
+++ Multithreading/ExecutorFramework/ExecutorSix.java
@@ -0,0 +1,42 @@
+package java_home.Multithreading.ExecutorFramework;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorSix {
+
+	public static void main(String[] args) {
+		//SingleThreadExecutor
+		
+		
+		ExecutorService ex=Executors.newSingleThreadExecutor();
+		
+		ex.execute(()->{
+			System.out.println(Thread.currentThread().getName()+"TAsk1");
+			for(int i=0;i<5;i++)
+			{
+				System.out.println(i);
+			}
+		});
+		
+		ex.execute(()->{
+			System.out.println(Thread.currentThread().getName()+"TAsk2");
+			for(int i=0;i<5;i++)
+			{
+				System.out.println(i);
+			}
+		});
+		
+		ex.execute(()->{
+			System.out.println(Thread.currentThread().getName()+"TAsk3");
+			for(int i=0;i<5;i++)
+			{
+				System.out.println(i);
+			}
+		});
+		
+		
+		
+	}
+
+}
